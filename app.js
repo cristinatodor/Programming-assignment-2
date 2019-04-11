e3a616f390c15ffb35c8aeb30bd5238d44b75d57
criiss31 <cristina.todor99@gmail.com>
2019-04-11 22:17:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ app.use(express.static('client'));
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 let aboutLocation = ["Geographically, it is roughly equidistant from Bucharest, Budapest and Belgrade. Located in the Somesul Mic River valley, the city is considered the unofficial capital to the historical province of Transylvania."];
 
 app.get('/about-location', function (req, resp){
