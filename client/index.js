526d42e8e1f8fd17b48a5d76c1e2ef1b6ae4b43e
criiss31 <cristina.todor99@gmail.com>
2019-04-11 21:44:04 +0300

    update add-review form

--- client/index.js
+++ client/index.js
@@ -139,50 +139,50 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	function writeEvents(events) {
 		let len = events.length; 
 		
-		document.getElementById('event-title1').innerHTML = events[len-9].title;
-		document.getElementById('event-location1').innerHTML = "Location: " + events[len-9].location;
-		document.getElementById('event-date1').innerHTML = events[len-9].date;
-		document.getElementById('event-text1').innerHTML = events[len-9].text;
+		document.getElementById('event-title1').innerHTML = events[len-1].title;
+		document.getElementById('event-location1').innerHTML = "Location: " + events[len-1].location;
+		document.getElementById('event-date1').innerHTML = events[len-1].date;
+		document.getElementById('event-text1').innerHTML = events[len-1].text;
 		
-		document.getElementById('event-title2').innerHTML = events[len-8].title;
-		document.getElementById('event-location2').innerHTML = "Location: " + events[len-8].location;
-		document.getElementById('event-date2').innerHTML = events[len-8].date;
-		document.getElementById('event-text2').innerHTML = events[len-8].text;
+		document.getElementById('event-title2').innerHTML = events[len-2].title;
+		document.getElementById('event-location2').innerHTML = "Location: " + events[len-2].location;
+		document.getElementById('event-date2').innerHTML = events[len-2].date;
+		document.getElementById('event-text2').innerHTML = events[len-2].text;
 		
-		document.getElementById('event-title3').innerHTML = events[len-7].title;
-		document.getElementById('event-location3').innerHTML = "Location: " + events[len-7].location;
-		document.getElementById('event-date3').innerHTML = events[len-7].date;
-		document.getElementById('event-text3').innerHTML = events[len-7].text;	
+		document.getElementById('event-title3').innerHTML = events[len-3].title;
+		document.getElementById('event-location3').innerHTML = "Location: " + events[len-3].location;
+		document.getElementById('event-date3').innerHTML = events[len-3].date;
+		document.getElementById('event-text3').innerHTML = events[len-3].text;	
 		
-		document.getElementById('event-title4').innerHTML = events[len-6].title;
-		document.getElementById('event-location4').innerHTML = "Location: " + events[len-6].location;
-		document.getElementById('event-date4').innerHTML = events[len-6].date;
-		document.getElementById('event-text4').innerHTML = events[len-6].text;
+		document.getElementById('event-title4').innerHTML = events[len-4].title;
+		document.getElementById('event-location4').innerHTML = "Location: " + events[len-4].location;
+		document.getElementById('event-date4').innerHTML = events[len-4].date;
+		document.getElementById('event-text4').innerHTML = events[len-4].text;
 			
 		document.getElementById('event-title5').innerHTML = events[len-5].title;
 		document.getElementById('event-location5').innerHTML = "Location: " + events[len-5].location;
 		document.getElementById('event-date5').innerHTML = events[len-5].date;
 		document.getElementById('event-text5').innerHTML = events[len-5].text;
 			
-		document.getElementById('event-title6').innerHTML = events[len-4].title;
-		document.getElementById('event-location6').innerHTML = "Location: " + events[len-4].location;
-		document.getElementById('event-date6').innerHTML = events[len-4].date;
-		document.getElementById('event-text6').innerHTML = events[len-4].text;
+		document.getElementById('event-title6').innerHTML = events[len-6].title;
+		document.getElementById('event-location6').innerHTML = "Location: " + events[len-6].location;
+		document.getElementById('event-date6').innerHTML = events[len-6].date;
+		document.getElementById('event-text6').innerHTML = events[len-6].text;
 			
-		document.getElementById('event-title7').innerHTML = events[len-3].title;
-		document.getElementById('event-location7').innerHTML = "Location: " + events[len-3].location;
-		document.getElementById('event-date7').innerHTML = events[len-3].date;
-		document.getElementById('event-text7').innerHTML = events[len-3].text;
+		document.getElementById('event-title7').innerHTML = events[len-7].title;
+		document.getElementById('event-location7').innerHTML = "Location: " + events[len-7].location;
+		document.getElementById('event-date7').innerHTML = events[len-7].date;
+		document.getElementById('event-text7').innerHTML = events[len-7].text;
 			
-		document.getElementById('event-title8').innerHTML = events[len-2].title;
-		document.getElementById('event-location8').innerHTML = "Location: " + events[len-2].location;
-		document.getElementById('event-date8').innerHTML = events[len-2].date;
-		document.getElementById('event-text8').innerHTML = events[len-2].text;
+		document.getElementById('event-title8').innerHTML = events[len-8].title;
+		document.getElementById('event-location8').innerHTML = "Location: " + events[len-8].location;
+		document.getElementById('event-date8').innerHTML = events[len-8].date;
+		document.getElementById('event-text8').innerHTML = events[len-8].text;
 			
-		document.getElementById('event-title9').innerHTML = events[len-1].title;
-		document.getElementById('event-location9').innerHTML = "Location: " + events[len-1].location;
-		document.getElementById('event-date9').innerHTML = events[len-1].date;
-		document.getElementById('event-text9').innerHTML = events[len-1].text;
+		document.getElementById('event-title9').innerHTML = events[len-9].title;
+		document.getElementById('event-location9').innerHTML = "Location: " + events[len-9].location;
+		document.getElementById('event-date9').innerHTML = events[len-9].date;
+		document.getElementById('event-text9').innerHTML = events[len-9].text;
 	};
 	
 	window.addEventListener("load", async function(event){
@@ -206,35 +206,35 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	function writeReviews(reviews) {
 		let len = reviews.length; 
 			
-		document.getElementById('review-title1').innerHTML = reviews[len-6].title;
-		document.getElementById('review-author1').innerHTML = "Posted by " + reviews[len-6].author;
-		document.getElementById('review-date1').innerHTML = reviews[len-6].date;
-		document.getElementById('review-text1').innerHTML = reviews[len-6].text;
+		document.getElementById('review-title1').innerHTML = reviews[len-1].title;
+		document.getElementById('review-author1').innerHTML = "Posted by " + reviews[len-1].author;
+		document.getElementById('review-date1').innerHTML = reviews[len-1].date;
+		document.getElementById('review-text1').innerHTML = reviews[len-1].text;
 			
-		document.getElementById('review-title2').innerHTML = reviews[len-5].title;
-		document.getElementById('review-author2').innerHTML = "Posted by " + reviews[len-5].author;
-		document.getElementById('review-date2').innerHTML = reviews[len-5].date;
-		document.getElementById('review-text2').innerHTML = reviews[len-5].text;
+		document.getElementById('review-title2').innerHTML = reviews[len-2].title;
+		document.getElementById('review-author2').innerHTML = "Posted by " + reviews[len-2].author;
+		document.getElementById('review-date2').innerHTML = reviews[len-2].date;
+		document.getElementById('review-text2').innerHTML = reviews[len-2].text;
 			
-		document.getElementById('review-title3').innerHTML = reviews[len-4].title;
-		document.getElementById('review-author3').innerHTML = "Posted by " + reviews[len-4].author;
-		document.getElementById('review-date3').innerHTML = reviews[len-4].date;
-		document.getElementById('review-text3').innerHTML = reviews[len-4].text;
+		document.getElementById('review-title3').innerHTML = reviews[len-3].title;
+		document.getElementById('review-author3').innerHTML = "Posted by " + reviews[len-3].author;
+		document.getElementById('review-date3').innerHTML = reviews[len-3].date;
+		document.getElementById('review-text3').innerHTML = reviews[len-3].text;
 			
-		document.getElementById('review-title4').innerHTML = reviews[len-3].title;
-		document.getElementById('review-author4').innerHTML = "Posted by " + reviews[len-3].author;
-		document.getElementById('review-date4').innerHTML = reviews[len-3].date;
-		document.getElementById('review-text4').innerHTML = reviews[len-3].text;
+		document.getElementById('review-title4').innerHTML = reviews[len-4].title;
+		document.getElementById('review-author4').innerHTML = "Posted by " + reviews[len-4].author;
+		document.getElementById('review-date4').innerHTML = reviews[len-4].date;
+		document.getElementById('review-text4').innerHTML = reviews[len-4].text;
 			
-		document.getElementById('review-title5').innerHTML = reviews[len-2].title;
-		document.getElementById('review-author5').innerHTML = "Posted by " + reviews[len-2].author;
-		document.getElementById('review-date5').innerHTML = reviews[len-2].date;
-		document.getElementById('review-text5').innerHTML = reviews[len-2].text;
-			
-		document.getElementById('review-title6').innerHTML = reviews[len-1].title;
-		document.getElementById('review-author6').innerHTML = "Posted by " + reviews[len-1].author;
-		document.getElementById('review-date6').innerHTML = reviews[len-1].date;
-		document.getElementById('review-text6').innerHTML = reviews[len-1].text;
+		document.getElementById('review-title5').innerHTML = reviews[len-5].title;
+		document.getElementById('review-author5').innerHTML = "Posted by " + reviews[len-5].author;
+		document.getElementById('review-date5').innerHTML = reviews[len-5].date;
+		document.getElementById('review-text5').innerHTML = reviews[len-5];
+		
+		document.getElementById('review-title6').innerHTML = reviews[len-6].title;
+		document.getElementById('review-author6').innerHTML = "Posted by " + reviews[len-6].author;
+		document.getElementById('review-date6').innerHTML = reviews[len-6].date;
+		document.getElementById('review-text6').innerHTML = reviews[len-6].text;
 	};
 	window.addEventListener("load", async function(event){
 		try {
@@ -318,8 +318,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		document.getElementById('eat-content').innerHTML = "";
 	});
 	
-	document.getElementById('send-review').addEventListener('click', async function(event) {
+	document.getElementById('form-add-review').addEventListener('submit', async function(event) {
 		event.preventDefault();
+		alert("ooooo");
 		
 		try {
 			let add_review_title = document.getElementById('add_review_title').value;
@@ -360,23 +361,3 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 
-/*document.getElementById('add-review').addEventListener('submit', async function(event){
-  event.preventDefault();
-  
-  try{
-    let potato_type = document.getElementById('potato_type').value;
-    let response = await fetch('http://127.0.01:8080/add',
-                               {
-                                 method: "POST",
-                                 headers: {
-                                   "Content-Type": "application/x-www-form-urlencoded"
-                                 },
-                                 body: "potato_type=" + potato_type
-                               });
-    if(!response.ok){
-      throw new Error("problem adding potato" + response.code);
-    }
-  } catch (error) {
-    alert ("problem: " + error);
-  } 
-}); */
