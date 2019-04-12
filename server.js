abd97b5f936f717e87bec7c7bb1c9acdfb71ac69
criiss31 <44347444+criiss31@users.noreply.github.com>
2019-04-12 14:11:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,6 @@
+
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
+var ip = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1';
+
 app = require("./app");
-app.listen(8080)
+app.listen(port, ip)
